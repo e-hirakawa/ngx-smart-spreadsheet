4fd2773f761cc7553b800ffe073954b9c83fc74d
EIJI HIRAKAWA <e.hirakawa@gmail.com>
2021-08-09 21:48:17 +0900

    Fixed bugs

--- projects/ngx-smart-spreadsheet/src/public-api.ts
+++ projects/ngx-smart-spreadsheet/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of ngx-smart-spreadsheet
  */
 
+export * from './lib/spreadsheet-settings';
 export * from './lib/ngx-smart-spreadsheet.component';
 export * from './lib/ngx-smart-spreadsheet.module';
-export * from './lib/ngx-smart-spreadsheet.service';
-export * from './lib/spreadsheet-settings';
